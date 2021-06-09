4fb2293fd61bb9e2f26659dafaa9164d365dbdd1
robocop@imac <robo@imac.T5>
2021-06-09 13:31:37 +0300

    works to produce pdfs in multiple pages

--- pie charts.R
+++ pie charts.R
@@ -1,34 +1,45 @@
+# Pies of mutations
 rm(list = ls())
-DF <- readRDS("/Users/deepankar/OneDrive - O365 Turun yliopisto/Git/GitHub/websites/eleniuslabtools.utu.fi/shiny-apps/COSMIC/HotspotMutations/DFa/20210603.FrequencyMutations.byTissue.RDS")
+setwd("/Users/deepankar/OneDrive - O365 Turun yliopisto/Klaus lab/Manuscripts/Hotspot Explorer/Data/Pies/RAW")
 
-for(i in seq(5,ncol(DF))){
-  sub <- na.omit(DF[,c(1,2,i)])
-  sub <- sub[order(sub[,3],decreasing = T),]
-  # Others cannot be represented as a slice
-  threshold <- 20;sum(sub[1:threshold,3]);sum(sub[threshold+1:nrow(sub),3],na.rm = T)
+DF <- readRDS("/Users/deepankar/OneDrive - O365 Turun yliopisto/Git/GitHub/websites/eleniuslabtools.utu.fi/shiny-apps/COSMIC/HotspotMutations/data/20210603.FrequencyMutations.byTissue.RDS")
+colnames(DF)[3] <- "all"
+
+pdf("pies.pdf",height = 5,width = 6,onefile = T)
+for(i in c(3,5:ncol(DF))){
+  # i <- 21
+  print(colnames(DF)[i])
+  sub <- na.omit(DF[,c(1,i)])
+  sub <- sub[order(sub[,2],decreasing = T),]
+  colnames(sub)[2] <- "count"
+  threshold <- 20
   
   # Trying top N
-  threshold <- 20
-  topN <- sub[1:threshold,]
-  colnames(topN) <- c("Gene","Mutation","count")
-  pie_table <- aggregate(topN$count~topN$Gene,FUN=sum)
-  colnames(pie_table) <- c("Gene","count")
-  pie_table <- pie_table[order(pie_table[,2],decreasing = T),]
-  
-  # Trying to work with larger lists
-  # pie_table[threshold+1,] <- list("others",sum(pie_table$count[10:nrow(pie_table)]))
-  # pie_table <- pie_table[c(1:9,nrow(pie_table)),]
+  pie_table_all <- aggregate(sub$count~sub$Gene,FUN=sum)
+  colnames(pie_table_all) <- c("Gene","count")
+  pie_table_all <- pie_table_all[order(pie_table_all$count,decreasing = T),]
+  threshold <- min(threshold,length(pie_table_all$Gene))
+  pie_table <- pie_table_all[1:threshold,]
+  pie_table[(threshold+1),] <- list("Others",sum(pie_table_all$count[(threshold+1):length(pie_table_all$count)]))
+  rm(pie_table_all,sub);gc()
+  sliceColors <- rep(NA,length(pie_table$Gene))
+  idx <- which(pie_table$Gene == "Others")
+  sliceColors[idx] <- "#c7c7c7"
+  sliceColors[-idx] <- viridis::plasma(length(pie_table$Gene[-idx]),direction = 1)
+  names(sliceColors) <- pie_table$Gene
   
   library(ggplot2)
-  pie <- ggplot(pie_table,aes(x="",y=count,fill=reorder(Gene,count)))+geom_bar(stat="identity", width=1, color="white") + theme_void() + scale_fill_viridis_d(option = "plasma",direction = -1) + theme(legend.position="bottom",legend.text=element_text(family="serif")) + guides(fill = guide_legend(title = "Genes", title.position = "left",title.theme = element_text(family="serif", face = "italic", angle = 0)))
+   pie_1 <- ggplot(pie_table[-21,],aes(x="",y=count,fill=reorder(Gene,-count)))+geom_bar(stat="identity", width=1, color="white") + theme_void() + scale_fill_manual(values= sliceColors) + theme(legend.position="none") + coord_polar(theta = "y",direction = -1)
   
-  print(pie + coord_polar(theta = "y")) # same as # pie + coord_polar(theta = "y", start=0,direction = 1)
+  pie_2 <- ggplot(pie_table,aes(x="",y=count,fill=reorder(Gene,-count)))+geom_bar(stat="identity", width=1, color=NA) + theme_void() + scale_fill_manual(values= sliceColors) + theme(legend.position="right",legend.text=element_text(family="serif",size=8)) + guides(fill = guide_legend(title = "Genes", title.position = "top", byrow = T, nrow = 21, title.theme = element_text(family="serif", face = "italic", angle = 0), override.aes = list(size = 1))) + coord_polar(theta = "y",direction = -1)
   
-}
-# pie + coord_polar()
-
-
-
-
-
-rm(pie_table)
+  pie <- gridExtra::grid.arrange(pie_1,
+                           pie_2,
+                           ncol=2,
+                           nrow=1,
+                           widths = c(2, 5),
+                           top = grid::textGrob(label = paste0(gsub("_"," ",colnames(DF)[i],fixed=T)," (n = ",prettyNum(sum(pie_table$count),big.mark = " ",scientific = F),")"), gp = grid::gpar(fontsize=18, font=4, fontfamily="serif"),just = "right",y = -1))
+  
+  print(pie)
+  
+} ; dev.off()
