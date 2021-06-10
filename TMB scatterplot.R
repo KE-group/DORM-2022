9bdf9d29c30ab2b1cd252223fafc5919988ce696
robocop@imac <robo@imac.T5>
2021-06-10 13:39:55 +0300

    benchmarking

--- TMB scatterplot.R
+++ TMB scatterplot.R
@@ -39,3 +39,37 @@ ggplot(data = Stats,aes(y=count,x=tissue))+geom_jitter(alpha=0.25)+scale_y_conti
 
 # ggsave("/Users/deepankar/OneDrive - O365 Turun yliopisto/Klaus lab/Manuscripts/Hotspot Explorer/Data/Barplots/SampleCounts.svg",width = 8,height = 5)
 
+
+
+# ------- Benchmarking -------
+
+# library(microbenchmark)
+# source("https://raw.githubusercontent.com/dchakro/shared_Rscripts/master/summarySE.R")
+# 
+# size <- 500000; bmark <- microbenchmark(
+#   "plyr" = {
+#     var1 <- plyr::count(dataDF[1:size,],"mutID")
+#   },
+#   "table" = {
+#     var2 <- data.frame(table(dataDF$mutID[1:size]),stringsAsFactors = F)
+#   },
+#   data.table = { # Fastest
+#     var3 <- data.table::as.data.table(dataDF[1:size,])[,.(.N), by = mutID]
+#   }
+#   ,times = 10)
+# results <- summarySE(bmark,measurevar = "time",groupvars = "expr",statistic = "mean")
+# 
+# 
+# var3 <- as.data.frame(var3)
+# colnames(var2) <- colnames(var1)
+# colnames(var3) <- colnames(var1)
+# var2$mutID <- as.character(var2$mutID)
+# 
+# var1 <- var1[order(var1$freq,var1$mutID,decreasing = T),]
+# var2 <- var2[order(var2$freq,var2$mutID,decreasing = T),]
+# var3 <- var3[order(var3$freq,var3$mutID,decreasing = T),]
+# 
+# all(var1$freq==var2$freq)
+# all(var1$mutID==var2$mutID)
+# all(var1$freq==var3$freq)
+# all(var1$mutID==var3$mutID)
