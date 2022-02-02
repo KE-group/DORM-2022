03736ee1fdc48dbd17d6a6b7260609fa0e170407
robocop@imac <robo@imac.T5>
2022-02-02 13:09:32 +0200

    incorporating stats from "discarded" samples

--- Figure S2.R
+++ Figure S2.R
@@ -3,44 +3,66 @@ library(data.table)
 rm(list=ls());gc()
 # ---> Figure S2 <----
 
+# Generating dataDF 
+# source("https://gist.githubusercontent.com/dchakro/8b1e97ba6853563dd0bb5b7be2317692/raw/parallelRDS.R")
+# muts <- readRDS.gz("/Users/deepankar/OneDrive - O365 Turun yliopisto/ExtraWorkSync/Klaus-Lab-Data/Big Data/COSMIC/v95/Full_Database/4.COSMIC.all.coding.Mutatations.RDS")
+# 
+# muts[, Mutation.AA := stringi::stri_replace_first_fixed(str = Mutation.AA,
+#                                                         pattern = "p.",
+#                                                         replacement = "")]
+# 
+# muts[, Mutation.AA := stringi::stri_replace_all_regex(str = Mutation.AA,
+#                                                       pattern = "\\*",
+#                                                       replacement = "X")]
+# 
+# muts[, mutID := paste(Gene.name,Mutation.AA,sep="=")]
+# 
+# output <- muts[,.(.N), by = mutID]
+# data.table::setnames(output, "N", "freq")
+# singleOccurances <- output[freq==1, .(mutID)]
+# rm(output)
+# 
+# '%nin%' <- Negate('%in%')
+# dataDF <- muts[, .(Sample.name, mutID, Primary.site)]
+# dataDF <- dataDF[mutID %nin% singleOccurances$mutID, ]
+# rm("%nin%", singleOccurances, muts)
+# gc()
+
+# saveRDS.gz(dataDF, file="/Users/deepankar/OneDrive - O365 Turun yliopisto/Klaus lab/Manuscripts/DORM database/Data/COSMIC_v95_R_DT/RecurrentMuts.dataDF.RDS")
+
+# Pre-load data
 source("https://gist.githubusercontent.com/dchakro/8b1e97ba6853563dd0bb5b7be2317692/raw/parallelRDS.R")
-muts <- readRDS.gz("/Users/deepankar/OneDrive - O365 Turun yliopisto/ExtraWorkSync/Klaus-Lab-Data/Big Data/COSMIC/v95/Full_Database/4.COSMIC.all.coding.Mutatations.RDS")
-rm(loadRDS,readRDS.gz,writeRDS,saveRDS.gz)
 
-muts[, Mutation.AA := stringi::stri_replace_first_fixed(str = Mutation.AA,
-                                                        pattern = "p.",
-                                                        replacement = "")]
+dataDF <- readRDS.gz("/Users/deepankar/OneDrive - O365 Turun yliopisto/Klaus lab/Manuscripts/DORM database/Data/COSMIC_v95_R_DT/RecurrentMuts.dataDF.RDS")
+fullData <- readRDS.gz("/Users/deepankar/OneDrive - O365 Turun yliopisto/ExtraWorkSync/Klaus-Lab-Data/Big Data/COSMIC/v95/Full_Database/1_AllsamplesMinInfo.RDS")
 
-muts[, Mutation.AA := stringi::stri_replace_all_regex(str = Mutation.AA,
-                                                      pattern = "\\*",
-                                                      replacement = "X")]
+# <---- Purging irrelevant entries from data ---->
+# Removing ENSEMBL transcripts and retaining just the genes
+fullData <- fullData[!grepl("_ENST", Gene.name, fixed = T), ]
+# Removing mutations annotated as ? in amino acid change
+fullData <- fullData[!stringi::stri_detect_regex(str = Mutation.AA, pattern = "^p.\\?" ),]
+# Removing Synonumous mutations
+fullData <- fullData[!grepl(pattern = '=', x = Mutation.AA, fixed = T), ]
 
-muts[, mutID := paste(Gene.name,Mutation.AA,sep="=")]
-
-output <- muts[,.(.N), by = mutID]
-data.table::setnames(output, "N", "freq")
-singleOccurances <- output[freq==1, .(mutID)]
-rm(output)
-
-'%nin%' <- Negate('%in%')
-dataDF <- muts[, .(Sample.name, mutID, Primary.site)]
-dataDF <- dataDF[mutID %nin% singleOccurances$mutID, ]
-rm("%nin%",singleOccurances, muts)
-gc()
+### Begin plotting
 
 # ---> Figure S2 A : No. of samples with recurrent mutations by tissue type <----
-rm(list = ls()[! ls() %in% c("dataDF")])
+rm(list = ls()[! ls() %in% c("dataDF","fullData")]) ; gc()
 SampleDF <- unique(dataDF[,.(Sample.name, Primary.site)])
 Stats <- SampleDF[,.(.N), by = Primary.site]
 colnames(Stats) <- c("tissue","count")
 setorder(Stats, -count)
-allStats <- readRDS("UnfilteredSampleCount.RDS")
-Stats <- merge.data.table(x = allStats, y = Stats, by = 'tissue')
+
+SampleDF <- unique(fullData[,.(Sample.name, Primary.site)])
+allStats <- SampleDF[,.(.N), by = Primary.site]
+colnames(allStats) <- c("tissue","count")
+
+# rm(loadRDS,readRDS.gz,writeRDS,saveRDS.gz)
+
+Stats <- merge.data.table(x = allStats, y = Stats, by = "tissue")
 Stats[, tissue := gsub("_", " ", tissue)]
 colnames(Stats)[2:3] <- c("All","Recurrent")
 setorder(Stats,-All)
-rm(allStats,dataDF,SampleDF)
-gc()
 
 Stats <- reshape2::melt(Stats)
 colnames(Stats)[2:3] <- c("Type","count")
@@ -58,11 +80,12 @@ ggplot(data = Stats, aes(x = reorder(tissue,-count),y = count,
                          fill=Type))+
   geom_col(width=0.75, position = "identity")+
   xlab("Tissue of origin of cancer")+
-  ylab("Number of samples\nwith recurrent mutations")+
+  ylab("Number of samples")+
   customtheme +
   scale_y_continuous(expand = c(0, 0))+
   scale_fill_manual(values = c("#c7c7c7","#000000"),
-                    labels=c("Unique","Recurrent"))+
+                    labels=c("Unique","Recurrent"),
+                    name="Type of \nSample")+
   theme(legend.position="right",
         legend.text=element_text(family="serif",
                                  size=11),
@@ -77,14 +100,26 @@ ggsave(
 
 
 # ---> Figure S2 B : No. of Recurrent mutations by cancer type <----
-rm(list = ls()[! ls() %in% c("dataDF", "DC_theme_generator")])
+rm(list = ls()[! ls() %in% c("dataDF","fullData", "DC_theme_generator")]);gc()
 
 Stats <- dataDF[,.(.N), by = Primary.site]
 colnames(Stats) <- c("tissue","count")
-setorder(Stats, -count)
 Stats[, tissue := gsub("_", " ", tissue)]
 
-source('https://raw.githubusercontent.com/dchakro/ggplot_themes/master/DC_theme_generator.R')
+allStats <- fullData[,.(.N), by = Primary.site]
+colnames(allStats) <- c("tissue","count")
+
+Stats <- merge.data.table(x = allStats, y = Stats, by = "tissue")
+Stats[, tissue := gsub("_", " ", tissue)]
+colnames(Stats)[2:3] <- c("All","Recurrent")
+setorder(Stats,-All)
+
+Stats <- reshape2::melt(Stats)
+colnames(Stats)[2:3] <- c("Type","count")
+
+if(!any(grepl("DC_theme_generator",x = ls()))){
+  source('https://raw.githubusercontent.com/dchakro/ggplot_themes/master/DC_theme_generator.R')  
+}
 customtheme <- DC_theme_generator(type = 'L',
                                   legend = 'F',
                                   ticks = 'out',
@@ -94,14 +129,30 @@ customtheme <- DC_theme_generator(type = 'L',
                                   fontsize.cex = 1.2,
                                   ax.fontstyle = "italic")
 options(scipen=100000)
-ggplot(data = Stats, aes(y=count,
-                         x=reorder(tissue, -count)))+
-  geom_col(fill="#000000",
-           width=0.75)+
-  customtheme+
-  scale_y_continuous(expand = c(0,0))+
+
+ggplot(data = Stats, aes(x = reorder(tissue,-count),y = count,
+                         fill=Type))+
+  geom_col(width=0.75, position = "identity")+
   xlab("Tissue of origin of cancer")+
-  ylab("Number of recurrent mutations")
+  ylab("Number of mutations")+
+  customtheme +
+  scale_y_continuous(expand = c(0, 0))+
+  scale_fill_manual(values = c("#c7c7c7","#000000"),
+                    labels=c("Unique","Recurrent"),
+                    name="Type of \nMutation")+
+  theme(legend.position="right",
+        legend.text=element_text(family="serif",
+                                 size=11),
+        legend.key.size = unit(0.5, "lines"))
+
+# ggplot(data = Stats, aes(y=count,
+#                          x=reorder(tissue, -count)))+
+#   geom_col(fill="#000000",
+#            width=0.75)+
+#   customtheme+
+#   scale_y_continuous(expand = c(0,0))+
+#   xlab("Tissue of origin of cancer")+
+#   ylab("Number of recurrent mutations")
 
 
 ggsave(
@@ -112,7 +163,7 @@ ggsave(
 )
 
 # ---> Figure S2 C : TMB plot <----
-rm(list = ls()[! ls() %in% c("dataDF", "DC_theme_generator")])
+rm(list = ls()[! ls() %in% c("dataDF", "fullData", "DC_theme_generator")])
 
 Stats <- dataDF[,.(.N), by = Sample.name]
 colnames(Stats) <- c("Sample.name","count")
@@ -123,7 +174,10 @@ Sample_Tissue_Map <- unique(dataDF[,.(Sample.name, Primary.site)])
 Stats[, tissue := Sample_Tissue_Map$Primary.site[match(x = Stats$Sample.name, table = Sample_Tissue_Map$Sample.name)]]
 Stats[, tissue := gsub("_", " ", tissue)]
 
-source('https://raw.githubusercontent.com/dchakro/ggplot_themes/master/DC_theme_generator.R')
+if(!any(grepl("DC_theme_generator",x = ls()))){
+  source('https://raw.githubusercontent.com/dchakro/ggplot_themes/master/DC_theme_generator.R')  
+}
+
 customtheme <- DC_theme_generator(type = 'L',
                                   legend = 'F',
                                   ticks = 'out',
