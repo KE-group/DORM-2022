809ca657df095d7d6895c387272710f8af8e02a8
robocop@imac <robo@imac.T5>
2022-09-06 12:13:52 +0300

    working code

--- gw vs targ/5. Scatterplot_gw_vs_targ by tissue.R
+++ gw vs targ/5. Scatterplot_gw_vs_targ by tissue.R
@@ -59,39 +59,35 @@ selection <- as.data.table(selection[-which(rowSums(selection[,c(2,3)])==0),])
 
 # # Removing genome-wide data from full and renaming to targeted seq
 # selection$full <- selection$full-ifelse(is.na(selection$gw),0,selection$gw) # creates problems with mutations like (ZNF814 A337V)
-setnames(selection,"full","targ")
 
-library(MASS)
-get_density <- function(x, y, ...) {
-  # Description: Get density of points in 2 dimensions.
-  # Source: https://slowkow.com/notes/ggplot2-color-by-density/
-  # Author: Kamil Slowikowski
-  
-  # @param x A numeric vector.
-  # @param y A numeric vector.
-  # @param n Create a square n by n grid to compute density.
-  
-  # @return The density within each square.
-  dens <- MASS::kde2d(x, y, ...)
-  ix <- findInterval(x, dens$x)
-  iy <- findInterval(y, dens$y)
-  ii <- cbind(ix, iy)
-  return(dens$z[ii])
-}
 
-selection[,density := get_density(x = gw, y = targ, n = N)]
+# selection[,density := get_density(x = gw, y = full, n = N)]
+tmp_data <- as.data.frame(stringi::stri_split_fixed(str = selection$MutID,pattern = "_",simplify = T))
+tmp_data <- within(tmp_data,  tissue <- paste(V2,V3,V4,V5,V6, sep=" "))
+selection$Mutant <- gsub("[=]"," ", tmp_data[,1])
+selection$tissue <- gsub("\\s+"," ",tmp_data$tissue)
+selection$tissue <- gsub("\\s$","",selection$tissue)
+rm(tmp_data)
+
+# levels(as.factor(selection$tissue))
 
 #-----------
 #  Plotting
+# colors <- viridis::turbo(length(unique(selection$tissue)))
+colors_data <- data.table::fread(file = "color_vector.tsv")
+colors <- colors_data$color
+names(colors) <- colors_data$tissue
+  
 library(ggplot2)
 source('https://raw.githubusercontent.com/dchakro/ggplot_themes/master/DC_theme_generator.R')
 customtheme <- DC_theme_generator(type = "L",legend = F)
-ggplot(data = selection, aes(x=gw, y=targ, color=density))+
-  geom_point(alpha=0.75,size=1)+
+ggplot(data = selection, aes(x=gw, y=full, color=tissue))+
+  geom_smooth(method = "lm",formula = y ~ x, aes(group=1),se = F,na.rm = T,color="#BE0000")+
+  geom_abline(slope = 1,intercept = 0, linetype="dotted")+
+  geom_point(alpha=0.75,size=1.5)+
   xlab("Share in genome-wide screens (%)")+
-  ylab("Share in targeted screens (%)")+
-  scale_color_viridis_c(option = "plasma")+
-  geom_abline(slope = 1,intercept = 0)+
+  ylab("Share in complete data (%)")+
+  scale_color_manual(values=colors)+
   scale_x_continuous(expand=c(0,0),limits = c(0,100))+
   scale_y_continuous(expand=c(0,0),limits = c(0,100))+
   ggtitle(paste0("Top ",N, " Mutations"))+
@@ -103,37 +99,135 @@ ggsave(
   width = 5
 )
 
+# #---- Plotly figure
+df_gw_bak[, MutID:=paste0(Gene,"=",Mutation)]
+df_full_bak[, MutID:=paste0(Gene,"=",Mutation)]
+
+findCount_gw <- function(mutationID = NULL,
+                         sampleSet = NULL) {
+  tmp_data <- unlist(stringi::stri_split_fixed(str = mutationID, pattern = "_"))
+  Mutant <- tmp_data[1]
+  Tissue <- paste0(tmp_data[-1], collapse = "_")
+  if (sampleSet == "genome-wide") {
+    tryCatch(
+      expr = {
+        return(df_gw_bak[MutID == Mutant, get(Tissue)])
+        
+      },
+      error = function (e) {
+        message(paste("No entry found for", mutationID))
+        # Actions to take
+        return(NA)
+      },
+      finally = {
+        # print("Great Success!!")
+      }
+    )
+  } else if (sampleSet == "full-data") {
+    tryCatch(
+      expr = {
+        return(df_full_bak[MutID == Mutant, get(Tissue)])
+        
+      },
+      error = function (e) {
+        message(paste("No entry found for", mutationID))
+        # Actions to take
+        return(NA)
+      },
+      finally = {
+        # print("Great Success!!")
+      }
+    )
+  } else {
+    error("Acceptable data sets (param #2): genome-wide / full-data")
+  }
+}
+
+# Single-core
+# unlist(lapply(
+#   selection$MutID,
+#   FUN = function(X)
+#     findCount_gw(X, "full-data")
+# ))
+
+# Parallel processing
+selection$gw_count <- unlist(
+  parallel::mclapply(
+    X = selection$MutID,
+    FUN = function(X)
+      findCount_gw(X, "genome-wide"),
+    mc.cores = parallel::detectCores()
+  )
+)
+
+selection$full_count <-
+  unlist(
+    parallel::mclapply(
+      X = selection$MutID,
+      FUN = function(X)
+        findCount_gw(X, "full-data"),
+      mc.cores = parallel::detectCores()
+    )
+  )
+
+# paste0(formatC(signif(selection$gw,digits=3), digits=3,format="fg")," %")
+
 library(plotly)
 p <- plot_ly(
-  selection,
+  data=selection,
   alpha = 0.8,
   x =  ~ gw,
-  y =  ~ targ,
-  color = ~density,
-  colors = "plasma",
+  y =  ~ full,
+  color = ~tissue,
+  colors = colors,
   mode = "text",
-  text =  ~ paste("Mutant: ", gsub("[=_]"," ", MutID),
-                  "\nCount (G) = ", gw,
-                  "\nCount (T) = ", targ)) %>% # write counts
+  text =  ~ paste("Mutant: ", paste0(Mutant," / ",tissue),
+                  "\n(Full): ",
+                  paste0(formatC(signif(full, digits = 3), digits = 3, format = "fg"),
+                         " % (n=",
+                         full_count,
+                         ")"),
+                  "\n (GW): ", 
+                  paste0(formatC(signif(gw, digits = 3), digits = 3, format = "fg"), 
+                         " % (n=", 
+                         gw_count, 
+                         ")"))) %>% # write counts
   layout(shapes = list(list(
     type = "line", 
     x0 = 0, 
-    x1 = ~max(selection$gw, selection$targ), 
+    x1 = ~max(selection$gw, selection$full), 
     xref = "x",
     y0 = 0, 
-    y1 = ~max(selection$gw, selection$targ),
+    y1 = ~max(selection$gw, selection$full),
     yref = "y",
-    line = list(color = "black")
+    line = list(color = "black", dash="dot")
   ))) %>%
   add_markers(marker = list(size = 10)) %>%
   layout(
     title = paste0("<b>Top ",N, " Mutations</b>"),
     xaxis = list(title = paste0("<b>Share in genome-wide screens (%)</b>")),
-    yaxis = list(title = paste0("<b>Share in targeted screens (%)</b>"))
+    yaxis = list(title = paste0("<b>Share in combined screen data (%)</b>"))
   )
-hide_colorbar(p)
+
+# p %>% add_trace(x = ~full, y = fitted(lm(selection$gw~selection$full), mode = "lines"))
+  
+fv <- fitted(lm(selection$gw~selection$full))
+
+p %>%  add_lines(
+  x = selection$gw,
+  y = fv,
+  group = 1,
+  name = "",
+  line = list(
+    color = "#BE0000",
+    width = 0.5,
+    dash = "dot"
+  )
+)
+
+hide_legend(p)
 htmlwidgets::saveWidget(
-  widget = hide_colorbar(p),
+  widget = hide_legend(p),
   file = paste0("gg_Scatter_tissue_top", N, ".html"),
   selfcontained = T,
   libdir = NULL)
