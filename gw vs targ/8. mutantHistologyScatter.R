93c1221daefd555e59e99c55e0cd62c75d48a6a1
robocop@imac <robo@imac.T5>
2022-10-05 11:12:25 +0300

    use patchwork to fix plot size as a square

--- gw vs targ/8. mutantHistologyScatter.R
+++ gw vs targ/8. mutantHistologyScatter.R
@@ -1,13 +1,29 @@
+to_install <- c("patchwork")  ## Set packages to install here and run
+if (unname(Sys.info()["sysname"]) == "Darwin") {
+  CPU_cores <- as.integer(system(command = "sysctl -n hw.physicalcpu", intern = T))
+  ## For logical cores:
+  # CPU_cores <- system("sysctl -n hw.ncpu")
+} else if (unname(Sys.info()["sysname"]) == "Linux") {
+  CPU_cores <- as.integer(system(command = "nproc", intern = T))
+}
+utils::setRepositories(ind = c(1, 2, 3))
+missing_packages <-
+  to_install[!(to_install %in% installed.packages()[, "Package"])]
+if (length(missing_packages))
+  install.packages(missing_packages, type = "source", INSTALL_opts = "--byte-compile", Ncpus = CPU_cores)
+rm(missing_packages, to_install, CPU_cores)
+
 setwd("/Users/deepankar/OneDrive - O365 Turun yliopisto/Klaus lab/Manuscripts/DORM database/Data/adding Targ to genomewide/mutants/")
 dir.create("plots",showWarnings = F)
 library(data.table)
+library(patchwork)
 rm(list=ls()); gc()
 
 source("https://raw.githubusercontent.com/dchakro/shared_Rscripts/master/roundUp.R")
 
 library(ggplot2)
 source('https://raw.githubusercontent.com/dchakro/ggplot_themes/master/DC_theme_generator.R')
-customtheme <- DC_theme_generator(type = "L",legend = T)
+customtheme <- DC_theme_generator(type = "L",legend = F)
 
 files <- as.list(setdiff(list.files(), list.dirs(recursive = FALSE, full.names = FALSE)))
 #----------
@@ -39,7 +55,7 @@ saveScatterplots <- function(fileName){
         sep = "\t",
         skip = 0,
         header = T,
-        stringsAsFactors = T,
+        stringsAsFactors = F,
         showProgress = T,
         nThread = 1)
 
@@ -89,7 +105,7 @@ saveScatterplots <- function(fileName){
 #     customtheme
   
   myDT[is.na(myDT)]=0
-  ggplot(data <- myDT,aes(x=gw_percentage,
+  main_plot <- ggplot(data <- myDT,aes(x=gw_percentage,
                           y=full_percentage,
                           size=full_mutant_N,
                           alpha=gw_mutant_N,
@@ -104,25 +120,72 @@ saveScatterplots <- function(fileName){
     # scale_color_brewer(palette = color_palette,"Histology")+
     scale_colour_viridis_d(option = "turbo", "Histology", direction = -1)+
     scale_size("N (full) / size")+
-    scale_alpha("N (gw) / opacity",range = c(0.3, 1))+
-    customtheme
+    scale_alpha("N (gw) / opacity",range = c(0.3, 1))
   
-  ggsave(filename = paste0("plots/", gsub(
-    pattern = ".tsv",
-    replacement = ".pdf",
-    x = fileName 
-  )),device = cairo_pdf,
-  width = max(7, 0.16*max(nchar(as.character(myDT$Histology)),na.rm = T)),
-  height = 6)
+  scatterPlot=main_plot+customtheme
+  legend=ggpubr::as_ggplot(ggpubr::get_legend(main_plot))
+  
+  # Fix plot size and legend takes rest of the space.
+  # Plot fixed size, legend dynamic size.
+  export <-  scatterPlot + 
+    legend + 
+    plot_layout(nrow = 2, ncol=1,
+                heights = unit(c(8, 1), c('cm', 'null')))
+  ggsave(
+    plot = export,
+    filename = paste0(
+      "plots/",
+      gsub(
+        pattern = ".tsv",
+        replacement = ".pdf",
+        x = fileName
+      )
+    ),
+    device = cairo_pdf,
+    width = 5,
+    height = 11
+  )
+  
+  
+  # ggsave(
+  #   plot = gridExtra::marrangeGrob(
+  #     myplots,
+  #     layout_matrix = matrix(
+  #       data = 1:2,
+  #       nrow = 2,
+  #       ncol = 1,
+  #       byrow = T
+  #     ),
+  #     as.table = F, top = NA
+  #   ),
+  #   filename = paste0(
+  #     "plots/",
+  #     gsub(
+  #       pattern = ".tsv",
+  #       replacement = ".pdf",
+  #       x = fileName
+  #     )
+  #   ),
+  #   device = cairo_pdf,
+  #   width = 5,
+  #   height = 10 + (0.2*(length(unique(myDT$Histology))))
+  # )
   
   # print(paste(mutID,tissue,sep=" / "))
   return(NULL)
 }
 
-parallel::mclapply(
-  X = files,
+# parallel::mclapply(
+#   X = files,
+#   FUN = function(X)
+#     saveScatterplots(X),
+#   mc.cores = parallel::detectCores()
+# )
+
+
+lapply(
+  files,
   FUN = function(X)
-    saveScatterplots(X),
-  mc.cores = parallel::detectCores()
+    saveScatterplots(X)
 )
 
